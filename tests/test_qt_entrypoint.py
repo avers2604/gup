4e9a9cbe703510ebb31a4d0e96b12d02751f8d64
avers2604 <averkiev2604@gmail.com>
2026-09-12 02:01:25 +0300

    test: simplify Qt self-test doubles

--- tests/test_qt_entrypoint.py
+++ tests/test_qt_entrypoint.py
@@ -4,67 +4,80 @@ import getpass_qt.app as qt_app
 from getpass_qt.app import main
 
 
+class _SelfTestStack:
+    @staticmethod
+    def count():
+        return 8
+
+
+class _SelfTestPreview:
+    def __init__(self, events):
+        self._events = events
+        self.has_image = True
+
+    def set_pil_image(self, image):
+        self._events.append(("preview", image))
+        self.has_image = image is not None
+
+
+class _SelfTestPlate:
+    def __init__(self, events):
+        self._events = events
+
+    def setText(self, value):
+        self._events.append(("plate", value))
+
+
+class _SelfTestVehiclePage:
+    def __init__(self, events):
+        self.preview = _SelfTestPreview(events)
+        self.pass_fields = {
+            "first": {"plate": _SelfTestPlate(events)},
+        }
+
+
+class _SelfTestSidebar:
+    def __init__(self, window, events):
+        self._window = window
+        self._events = events
+
+    def request_route(self, route):
+        self._events.append(("route", route))
+        self._window.active_route = route
+
+
+class _SelfTestWindow:
+    events = []
+
+    def __init__(self, theme_manager, vehicle_viewmodel):
+        self.active_route = "dashboard"
+        self.stack = _SelfTestStack()
+        self.sidebar = _SelfTestSidebar(self, self.events)
+        self.vehicle_page = _SelfTestVehiclePage(self.events)
+
+    @staticmethod
+    def show():
+        pass
+
+    @staticmethod
+    def close():
+        pass
+
+
 def test_qt_self_test_returns_zero(monkeypatch):
     monkeypatch.setenv("QT_QPA_PLATFORM", "offscreen")
     assert main(["--self-test"]) == 0
 
 
-def test_qt_self_test_exercises_vehicle_route_and_binding(monkeypatch):
+def test_qt_self_test_exercises_debounced_vehicle_binding(monkeypatch):
     events = []
-
-    class FakeStack:
-        @staticmethod
-        def count():
-            return 8
-
-    class FakePreview:
-        def __init__(self):
-            self.has_image = True
-
-        def set_pil_image(self, image):
-            events.append(("preview", image))
-            self.has_image = image is not None
-
-    class FakePlate:
-        @staticmethod
-        def setText(value):
-            events.append(("plate", value))
-
-    class FakeVehiclePage:
-        def __init__(self):
-            self.preview = FakePreview()
-            self.pass_fields = {
-                "first": {"plate": FakePlate()},
-            }
-
-    class FakeSidebar:
-        def __init__(self, window):
-            self._window = window
-
-        def request_route(self, route):
-            events.append(("route", route))
-            self._window.active_route = route
-
-    class FakeWindow:
-        def __init__(self, theme_manager, vehicle_viewmodel):
-            self.active_route = "dashboard"
-            self.stack = FakeStack()
-            self.sidebar = FakeSidebar(self)
-            self.vehicle_page = FakeVehiclePage()
-
-        @staticmethod
-        def show():
-            pass
-
-        @staticmethod
-        def close():
-            pass
+    _SelfTestWindow.events = events
 
     def fake_wait_for_preview_refresh(window, app):
         events.append(("wait", 250))
         window.vehicle_page.preview.has_image = True
 
-    monkeypatch.setattr(qt_app, "MainWindow", FakeWindow)
+    monkeypatch.setattr(qt_app, "MainWindow", _SelfTestWindow)
     monkeypatch.setattr(
         qt_app,
         "_wait_for_preview_refresh",
