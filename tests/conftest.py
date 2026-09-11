37e1043f5da52f0995e0927a7704941906566e6a
avers2604 <averkiev2604@gmail.com>
2026-09-11 23:16:47 +0300

    test: keep Qt offscreen setup lint-clean

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
-
 import pytest
 
+os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
